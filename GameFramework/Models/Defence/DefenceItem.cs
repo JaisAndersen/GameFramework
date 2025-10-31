1f2b0d0f880a5d5cd7f27d5a0a5ce65148e60255
Jais Andersen <jais.h.sorensen@gmail.com>
2025-10-31 09:31:29 +0100

    DefenceItem added

---
+++ GameFramework/Models/Defence/DefenceItem.cs
@@ -0,0 +1,23 @@
+﻿using GameFramework.Worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.Models.Defence
+{
+    public class DefenceItem : WorldObject
+    {
+        public int ReduceHitPoint { get; set; }
+        public DefenceItem()
+        {
+            Name = string.Empty;
+            ReduceHitPoint = 0;
+        }
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(ReduceHitPoint)}={ReduceHitPoint.ToString()}}}";
+        }
+    }
+}

a44f46baa11848b693a9b9eb092130d79aa695f3
Jais Andersen <jais.h.sorensen@gmail.com>
2025-10-31 09:14:51 +0100

    WorldObject created

---
+++ GameFramework/Worlds/WorldObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.Worlds
+{
+    public class WorldObject
+    {
+        public string Name { get; set; }
+        public bool Lootable { get; set; }
+        public bool Removeable { get; set; }
+
+        public WorldObject()
+        {
+            Name = string.Empty;
+            Lootable = false;
+            Removeable = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(Lootable)}={Lootable}, {nameof(Removeable)}={Removeable}}}";
+        }
+    }"
+        }
+    }
+}

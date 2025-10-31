13c12ef489dfefb2bc22755ac44034cb0553e8b4
Jais Andersen <jais.h.sorensen@gmail.com>
2025-10-31 09:04:40 +0100

    Add project files.

---
+++ GameFramework/Worlds/World.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.Worlds
+{
+    public class World
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        private List<WorldObject> _worldObjects;
+        private List<Creature> _creatures;
+    }
+}

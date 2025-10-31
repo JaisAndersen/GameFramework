6e27a29eb0c198d12e7de4534e1ad0e24894a142
Jais Andersen <jais.h.sorensen@gmail.com>
2025-10-31 13:54:59 +0100

    Logger created

---
+++ GameFramework/GameFrameworkLogger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    public class GameFrameworkLogger
+    {
+        private static GameFrameworkLogger _instance = new();
+        public static GameFrameworkLogger Instance { get { return _instance; } }
+        private GameFrameworkLogger()
+        {
+            _ts = new TraceSource("GameFrameworkTraceSource", SourceLevels.All);
+            _ts.Switch = new SourceSwitch("GameFrameworkSourceSwitch", SourceLevels.All.ToString());
+        }
+        private TraceSource _ts;
+        private TraceListener? _debugListener = null;
+
+        public void AddListener(TraceListener listener)
+        {
+            _ts.Listeners.Add(listener);
+        }
+
+        public void RemoveListener(TraceListener listener)
+        {
+            _ts.Listeners.Remove(listener);
+        }
+
+        public void SetDefaultLevel(SourceLevels level)
+        {
+            _ts.Switch.Level = level;
+        }
+        public void SetDebugLogging()
+        {
+            if (_debugListener == null)
+            {
+                _debugListener = new ConsoleTraceListener();
+                _ts.Listeners.Add(_debugListener);
+            }
+        }
+        public void RemoveDebugLogging()
+        {
+            if (_debugListener != null)
+            {
+                _ts.Listeners.Remove(_debugListener);
+                _debugListener = null;
+            }
+        }
+        public void stop()
+        {
+            _ts.Close();
+        }
+
+        public void LogInformation(string message)
+        {
+            _ts.TraceEvent(TraceEventType.Information, 2, message);
+        }
+        public void LogWarning(string message)
+        {
+            _ts.TraceEvent(TraceEventType.Warning, 2, message);
+        }
+        public void LogError(string message)
+        {
+            _ts.TraceEvent(TraceEventType.Error, 2, message);
+        }
+        public void LogCritical(string message)
+        {
+            _ts.TraceEvent(TraceEventType.Critical, 2, message);
+        }
+    }
+}

e5e11305b45852c330b31058241e6d319c2efd83
Jais Andersen <jais.h.sorensen@gmail.com>
2025-11-02 21:50:19 +0100

    World Config created

---
+++ GameFramework/WorldConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    public class WorldConfig
+    {
+        private static WorldConfig _instance = new();
+        public static WorldConfig Instance { get { return _instance; } }
+
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public Dificulty Dificulty { get; set; }
+
+        private WorldConfig()
+        {
+            MaxX = 20;
+            MaxY = 20;
+            Dificulty = Dificulty.Medium;
+        }
+    }
+}

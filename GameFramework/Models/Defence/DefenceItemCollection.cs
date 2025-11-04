1ccb007800891ed5e30f787740dc4bbc7b2b99b6
Jais Andersen <jais.h.sorensen@gmail.com>
2025-11-04 12:22:53 +0100

    DefenceItems Composite class added.

---
+++ GameFramework/Models/Defence/DefenceItemCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.Models.Defence
+{
+    public class DefenceItemCollection
+    {  
+        private List<DefenceItem> items;
+        public DefenceItemCollection()
+        {
+            items = new List<DefenceItem>();
+        }
+        public void AddItem(DefenceItem item)
+        {
+            items.Add(item);
+        }
+        public void RemoveItem(DefenceItem item)
+        {
+            items.Remove(item);
+        }
+        public DefenceItem? GetItemByName(string name)
+        {
+            return items.FirstOrDefault(i => i.Name == name);
+        }
+        public List<DefenceItem> GetAllItems()
+        {
+            return new List<DefenceItem>(items);
+        }
+    }
+}

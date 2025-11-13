54da00891591ff18e0ff9f735d0416dd1001a2fc
Jais Andersen <jais.h.sorensen@gmail.com>
2025-11-13 12:38:06 +0100

    Classes for factory pattern added.

---
+++ GameFramework/ObjectFactory.cs
@@ -0,0 +1,14 @@
+﻿using GameFramework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    public abstract class ObjectFactory : IObjectFactory
+    {
+        
+    }
+}

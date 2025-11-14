c650d21b620f137a750e24d535ead6c25900b07a
Jais Andersen <jais.h.sorensen@gmail.com>
2025-11-14 14:42:07 +0100

    Main page comments added

---
+++ GameFramework/Documentation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+
+    /**
+    \mainpage GameFramework Documentation
+
+    Welcome to the GameFramework API docs.
+
+    \section overview Overview
+    This project contains game world objects, attack items, and related interfaces.
+
+    */
+        internal class Documentation
+    {
+        // This class is intentionally left empty.
+    }
+}

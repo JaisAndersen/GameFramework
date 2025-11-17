9452f6e94c273306643e485025ff25ed91b2ef61
Jais Andersen <jais.h.sorensen@gmail.com>
2025-11-17 10:50:00 +0100

    CordControlls added with Operation overload.

---
+++ GameFramework/CordsControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    public struct CordsControl
+    {
+        public int XCord { get; set; }
+        public int YCord { get; set; }
+
+        public CordsControl(int xCord, int yCord)
+        {
+            XCord = xCord;
+            YCord = yCord;
+        }
+        public static CordsControl operator +(CordsControl currentCord, CordsControl newCord)
+        {
+            return new CordsControl(currentCord.XCord + newCord.XCord, currentCord.YCord + newCord.YCord);
+        }
+        public static CordsControl operator -(CordsControl currentCord, CordsControl newCord)
+        {
+            return new CordsControl(currentCord.XCord - newCord.XCord, currentCord.YCord - newCord.YCord);
+        }
+        public override string ToString()
+        {
+            return $"{(XCord, YCord)}";
+        }
+    }
+}
